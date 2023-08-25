35f0947a0c91044364afbfcb66895ad3239bdf32
Nikesh Uprety <upretynikesh123@gmail.com>
2023-08-25 21:37:17 +0545

    TRY_DEPLOY_v3

--- Frontend/src/components/ProfileScreens/ReadListPage.js
+++ Frontend/src/components/ProfileScreens/ReadListPage.js
@@ -56,7 +56,7 @@ const ReadListPage = () => {
 
                     <div className="readList-top-block">
 
-                        <img src={`/userPhotos/${activeUser.photo}`} alt={activeUser.username} />
+                        <img src={`${BASE_URL}/userPhotos/${activeUser.photo}`} alt={activeUser.username} />
 
 
                         <div className='activeUser-info-wrapper'>
